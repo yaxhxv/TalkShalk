c27ef661a1cd81592aaa1b8eeb3241dca6db1d3a
yaxhxv <yashnawgiri45.com>
2025-04-18 10:23:19 +0530

    integrated Auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,21 +2,23 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import {connectDB }from "./lib/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+app.use(cookieParser())
 app.use("/api/auth", authRoutes);
 
  
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
-  connectDB();
+   connectDB();
 });
 
 
-//30:59cdsdc
+//58:00
